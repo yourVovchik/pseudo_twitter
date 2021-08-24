c3fa4bb9de9434eaf692ee90434beb2d8bdafb04
Zhelakovich_Vladimir <78911524+yourVovchik@users.noreply.github.com>
2021-08-24 13:18:00 +0300

    To start, run the file "role.sql" and "run.sql". DB pgsql. Login on the page ""/login" Login: user@mail.com , password : 123 Role: User; Login: admin@mail.com , password : 123 Role: Admin.

---
+++ src/main/java/com/example/pseudo_twitter/entity/message/Message.java
@@ -0,0 +1,12 @@
+package com.example.pseudo_twitter.entity.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String text;
+}
